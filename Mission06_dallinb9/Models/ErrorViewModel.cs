dcabbc677c093b56c94b02e3e3164ac335cc5f28
Dallin Blanc <dallinb94@gmail.com>
2023-02-13 15:38:04 -0700

    Add project files.

---
+++ Mission06_dallinb9/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Mission06_dallinb9.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
